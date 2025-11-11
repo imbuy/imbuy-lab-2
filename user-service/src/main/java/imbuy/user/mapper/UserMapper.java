fdb42b489ce7add3a6dade7c7cd5fc2688b26ece
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2025-11-11 21:52:10 +0300

    feat: add mapper for user service

---
+++ user-service/src/main/java/imbuy/user/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package imbuy.user.mapper;
+
+import imbuy.user.domain.User;
+import imbuy.user.dto.UserDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public UserDto mapToDto(User user) {
+        return new UserDto(
+                user.getId(),
+                user.getEmail(),
+                user.getUsername(),
+                user.getBalance() != null ? user.getBalance().toString() : "0.00"
+        );
+    }
+}
