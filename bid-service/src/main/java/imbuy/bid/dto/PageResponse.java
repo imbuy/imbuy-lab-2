28f72687927d964c1b0ad89ffca74de024c60467
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2025-11-13 00:14:43 +0300

    fix: delete unused page response at bid

--- bid-service/src/main/java/imbuy/bid/dto/PageResponse.java
+++ bid-service/src/main/java/imbuy/bid/dto/PageResponse.java
@@ -1,6 +1,5 @@
 package imbuy.bid.dto;
 
-import org.springframework.data.domain.Page;
 import java.util.List;
 
 public record PageResponse<T>(
@@ -9,14 +8,4 @@ public record PageResponse<T>(
         int page_size,
         boolean has_next,
         boolean has_previous
-) {
-    public static <T> PageResponse<T> of(Page<T> page) {
-        return new PageResponse<>(
-                page.getContent(),
-                page.getNumber(),
-                page.getSize(),
-                page.hasNext(),
-                page.hasPrevious()
-        );
-    }
-}
+) {}
