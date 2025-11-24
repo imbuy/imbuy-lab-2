c17444a2eb92879f053f1f6a23f868745bb60eb1
Шевченко Дарья <d.shevchenko@rivc-pulkovo.ru>
2025-11-24 23:59:35 +0300

    fix: fix configuration feign

---
+++ lot-service/src/main/java/imbuy/lot/client/FeignConfig.java
@@ -0,0 +1,17 @@
+package imbuy.lot.client;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
+
+@Configuration
+public class FeignConfig {
+    
+    @Bean
+    public Capability micrometerCapability(MeterRegistry meterRegistry) {
+        return new MicrometerCapability(meterRegistry);
+    }
+}
+
