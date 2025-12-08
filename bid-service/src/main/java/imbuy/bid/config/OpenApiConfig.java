7bea7f435bc2f54a5e38724ac09b020727d84a0b
Шевченко Дарья <d.shevchenko@rivc-pulkovo.ru>
2025-12-08 20:47:37 +0300

    fix: add servers for swagger

--- bid-service/src/main/java/imbuy/bid/config/OpenApiConfig.java
+++ bid-service/src/main/java/imbuy/bid/config/OpenApiConfig.java
@@ -2,8 +2,11 @@ package imbuy.bid.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -14,7 +17,10 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Bid Service API")
                         .version("1.0.0")
-                        .description("Bid management service API documentation"));
+                        .description("Bid management service API documentation"))
+                .servers(List.of(
+                        new Server().url("/")
+                ));
     }
 }
 
