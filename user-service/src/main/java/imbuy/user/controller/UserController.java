86f1aecf90035cf82d3be0cc24e52643c65dbfda
Шевченко Дарья <d.shevchenko@rivc-pulkovo.ru>
2025-12-11 20:20:09 +0300

    fix: circuit

--- user-service/src/main/java/imbuy/user/controller/UserController.java
+++ user-service/src/main/java/imbuy/user/controller/UserController.java
@@ -15,7 +15,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 @Tag(name = "User Management", description = "APIs for managing users")
 public class UserController {
